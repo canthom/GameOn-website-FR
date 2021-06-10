61bf93e54cc2f668c20fa32cc51a19cc759466f9
Thomas Can <can.thomas@orange.fr>
2021-06-10 13:25:53 +0200

    Refonte du code JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -41,12 +41,65 @@ const tourneys = document.getElementById('quantity');
 const radioLocations = document.querySelectorAll('input[type="radio"]');
 const conditions = document.getElementById('checkbox1');
 
-// ERROR MESSAGE
-const newElt = document.createElement('span');
-let elt = document.getElementsByClassName('formData');
-
-// SOUMETTRE LE FORMULAIRE
+// EVENT : SOUMETTRE LE FORMULAIRE
 submitModal.addEventListener('submit', function(e) {
+  // Vérification du prénom
+  if (firstName.value.length < 2) {
+    formData[0].dataset.error = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
+    formData[0].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[0].removeAttribute('data-error');
+    formData[0].removeAttribute('data-error-visible');
+  }
+
+  // Vérification du nom de famille
+  if (lastName.value.length < 2) {
+    formData[1].dataset.error = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+    formData[1].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[1].removeAttribute('data-error');
+    formData[1].removeAttribute('data-error-visible');
+  }
+
+  // Vérification de l'adresse mail
+  mailFormat = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if (!mailFormat.test(mailAdress.value)) {
+    formData[2].dataset.error = 'Veuillez entrer une adresse email valide.';
+    formData[2].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[2].removeAttribute('data-error');
+    formData[2].removeAttribute('data-error-visible');
+  }
+
+  // Vérification de la date de naissance
+  const birth = new Date(birthDate.value);
+  const today = new Date();
+
+  if  ((isNaN(Date.parse(birthDate.value))) || (birth > today)) {
+    formData[3].dataset.error = 'Veuillez entrer une date de naissance valide au format aaaa-mm-jj';
+    formData[3].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[3].removeAttribute('data-error');
+    formData[3].removeAttribute('data-error-visible');
+  }
+
+  // Vérification du nombre de tournois
+  tourneyFormat = /^[0-9]$/
+  if ((tourneys.value.length < 1) || (!tourneyFormat.test(tourneys.value))) {
+    formData[4].dataset.error = 'Vous devez insérez une valeur numérique.';
+    formData[4].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[4].removeAttribute('data-error');
+    formData[4].removeAttribute('data-error-visible');
+  }
+
+  // Vérification du choix de la ville
   let isLocationChecked;
   for (radioLocation of radioLocations) {
     if (radioLocation.checked) {
@@ -55,77 +108,88 @@ submitModal.addEventListener('submit', function(e) {
     }
   };
 
-  // REGULAR EXPRESSION
-  const mailFormat = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  if (isLocationChecked !== 1) {
+    formData[5].dataset.error = 'Vous devez insérez une valeur numérique.';
+    formData[5].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[5].removeAttribute('data-error');
+    formData[5].removeAttribute('data-error-visible');
+  }
 
-  // DATES
-  const birth = new Date(birthDate.value);
-  const today = new Date(); 
- 
-  if (firstName.value.length < 2) {
+  // Vérification des conditions d'utilisation
+  if (!(conditions.checked)) {
+    formData[6].dataset.error = "Vous devez accepter les conditions d'utilisation.";
+    formData[6].dataset.errorVisible = 'true';
+    
+  } else {
+    formData[6].removeAttribute('data-error');
+    formData[6].removeAttribute('data-error-visible');
+  }
+
+
+  // Comportement du formulaire
+  let hasError;
+  for (var i = 0; i < formData.length; i++) {
+    if (formData[i].hasAttribute('data-error')) {
+      hasError = 1;
+      break;
+    }
+  }
+
+  if (hasError === 1) {
     e.preventDefault();
-    elt[0].appendChild(newElt);
-    document.querySelector('div.formData > span').innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-    document.querySelector('div.formData > span').classList.add("error");
-    firstName.style.border = "red";
-  } else if (lastName.value.length < 2) {
-    e.preventDefault();
-    elt[1].appendChild(newElt);
-    document.querySelector('div.formData > span').innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-    document.querySelector('div.formData > span').classList.add("error");
-   } else if (!(mailAdress.value.match(mailFormat))) {
-    e.preventDefault();
-    elt[2].appendChild(newElt);
-    document.querySelector('div.formData > span').innerHTML = 'Cette adresse email est invalide!';
-    document.querySelector('div.formData > span').classList.add("error");
-  } else if ((birth > today) || (birthDate.value === "")) {
-    e.preventDefault();
-    elt[3].appendChild(newElt);
-    document.querySelector('div.formData > span').innerHTML = 'Vous devez entrer une date de naissance valide.';
-    document.querySelector('div.formData > span').classList.add("error");
-  } else if ((tourneys.value.length < 1) || (tourneys.value == NaN)) {
-    e.preventDefault();
-    elt[4].appendChild(newElt);
-    document.querySelector('div.formData > span').innerHTML = 'Vous devez insérez une valeur numérique.';
-    document.querySelector('div.formData > span').classList.add("error");
-  } else if (isLocationChecked !== 1) {
-    e.preventDefault();
-    elt[5].appendChild(newElt);
-    document.querySelector('div.formData > span').innerHTML = 'Vous devez sélectionner une ville.';
-    document.querySelector('div.formData > span').classList.add("error");
-    document.querySelector('div.formData > span').style.display = 'block';
-  } else if (!(conditions.checked)) {
-    e.preventDefault();
-    elt[6].insertBefore(newElt, document.getElementById('checkbox2'));
-    document.querySelector('div.formData > span').innerHTML = "Vous devez accepter les conditions d'utilisation.";
-    document.querySelector('div.formData > span').classList.add("error");
   } else {
     e.preventDefault();
+    closeModal();
+    document.forms[0].reset();
 
-    // CONFIRMATION WINDOW
-    document.querySelector('.modal-body > form').style.display = "none";
-    document.querySelector('.modal-body').style.height = "auto";
-    document.querySelector('.modal-body').style.display = "flex";
-    document.querySelector('.modal-body').style.flexDirection = "column";
-    document.querySelector('.modal-body').style.justifyContent = "center";
-    document.querySelector('.modal-body').style.alignItems = "center";
+  // Validation Box
+    const div = document.createElement('div');
+    document.querySelector('body').appendChild(div);
+    document.querySelector('body > div:last-child').classList.add('content', 'validBox');
+    document.querySelector('.validBox').style.width = '500px';
+    document.querySelector('.validBox').style.height = '600px';
+    document.querySelector('.validBox').style.position = 'absolute';
+    document.querySelector('.validBox').style.top = '50%';
+    document.querySelector('.validBox').style.left = '50%';
+    document.querySelector('.validBox').style.transform = 'translate(-50%, -50%)';
+    document.querySelector('.validBox').style.zIndex = '100';
+    document.querySelector('.validBox').style.display = 'block';
+    document.querySelector('.validBox').style.boxShadow = '0 10px 20px black';
 
-    // CONFIRMATION MESSAGE
-    document.querySelector('.modal-body').appendChild(newElt);
-    document.querySelector('.modal-body > span').innerHTML = "Merci ! Votre réservation a été reçue.";
-    document.querySelector('.modal-body > span').style.color = "white";
-    document.querySelector('.modal-body > span').style.fontSize = "36px";
+  // Validation Box Span Close
+    document.querySelector('.validBox').appendChild(document.createElement('span'));
+    document.querySelector('.validBox > span').classList.add('close');
+    document.querySelector('.validBox > span').addEventListener('click', function() {
+      document.querySelector('body').removeChild(div);
+    });
+
+  // Validation Box Content
+    document.querySelector('.validBox').appendChild(document.createElement('div'));
+    document.querySelector('.validBox > div').classList.add('modal-body', 'validMessage');
+    let newSpan = document.createElement('span');
+    document.querySelector('.validMessage').appendChild(newSpan);
+    document.querySelector('.validMessage').style.height = '80%';
+    document.querySelector('.validMessage').style.width = '80%';
+    document.querySelector('.validMessage').style.display = 'flex';
+    document.querySelector('.validMessage').style.flexDirection = 'column';
+    document.querySelector('.validMessage').style.alignItems = 'center';
+    document.querySelector('.validMessage').style.justifyContent = 'center';
+    document.querySelector('.validMessage > span').innerHTML = "Merci ! Votre réservation a été reçue.";
+    document.querySelector('.validMessage > span').style.fontSize = '36px';
+    document.querySelector('.validMessage > span').style.color = 'white';
+    document.querySelector('.validMessage > span').style.textAlign = 'center';
 
 
-    // CLOSE BUTTON
+  //  CLOSE BUTTON
     const btn = document.createElement("button");
-    document.querySelector('.modal-body').appendChild(btn);
-    document.querySelector('.modal-body button').innerHTML = "Fermer";
-    document.querySelector('.modal-body button').classList.add("btn-signup");
-
-    
-    document.querySelector('.modal-body button').addEventListener('click', function () {
-      closeModal();
+    document.querySelector('.validBox').appendChild(btn);
+    document.querySelector('.validBox > button').innerHTML = "Fermer";
+    document.querySelector('.validBox > button').style.margin = '0 auto';
+    document.querySelector('.validBox > button').classList.add("btn-signup");
+    document.querySelector('.validBox > button').addEventListener('click', function() {
+      document.querySelector('body').removeChild(div);
     });
   }
 });
